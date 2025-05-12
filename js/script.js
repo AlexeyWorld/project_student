9071797e2abb9fe821a251fcafe5ac0eafdb928e
AlexeyWorld <contract44fz@gmail.com>
2025-05-12 16:53:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 'use strict'
+
 document.addEventListener('DOMContentLoaded', () => {
+
     const intensiveImg = document.querySelector(".png");
+
     intensiveImg.addEventListener('mouseenter', () => { 
+
              console.log('Мышка наведена на изображение, показываем текст');
+
      });
+
+intensiveImg.addEventListener('mouseleave', () => {
+
+    console.log('Мышка покинула изображение, скрываем текст');
+
+});
+
+intensiveImg.addEventListener('click', () => {
+
+    alert('Изображение было кликнуто!');
+
+});
+
+const newElement = document.createElement('p');
+
+newElement.textContent = 'Это динамически добавленный текст.';
+
+intensiveImg.parentNode.insertBefore(newElement, intensiveImg.nextSibling);
+
+newElement.style.color = 'blue';
+
 });
     
        
