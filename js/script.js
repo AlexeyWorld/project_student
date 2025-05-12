969a9dc98411ff23474e41903d22be7fdf0eca5c
AlexeyWorld <contract44fz@gmail.com>
2025-05-12 16:08:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 'use strict'
-const intensiveImg = document.querySelector(".1.png");
+document.addEventListener("DOMContentLoaded", () => {
+     
+     const intensiveImg = document.querySelector(".1.png");
      intensiveImg.addEventListener('mouseenter', () => {
             console.log('Мышка наведена на изображение, показываем текст');
-
+     });
 });
